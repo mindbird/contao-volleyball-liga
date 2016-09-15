93743cf18274dcf321fabf8fa47815ba4441c15f
Wusch <florian.otto@mindbird.de>
2016-09-15 13:24:15 +0200

    Add composer support

---
+++ contao/config/autoload.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+    'mod_volleyball_liga_spielplan' => 'system/modules/volleyball-liga/templates',
+    'volleyball_liga_spielplan_tabellenzeile' => 'system/modules/volleyball-liga/templates',
+    'mod_volleyball_liga_tabelle' => 'system/modules/volleyball-liga/templates',
+    'volleyball_liga_tabelle_tabellenzeile' => 'system/modules/volleyball-liga/templates',
+));
